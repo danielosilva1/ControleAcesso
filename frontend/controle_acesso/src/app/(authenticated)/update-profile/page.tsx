2fa3d3d4a7acede74cdcbdf91afa886a36bdd427
dosilva <danielosilva@alu.ufc.br>
2024-05-19 17:04:19 -0300

    Implementa navegação entre os componentes da tela de atualização do perfil do usuário logado por meio da tecla enter

--- frontend/controle_acesso/src/app/(authenticated)/update-profile/page.tsx
+++ frontend/controle_acesso/src/app/(authenticated)/update-profile/page.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import styles from "./page.module.css";
 
 export default function updateProfile() {
@@ -12,6 +12,40 @@ export default function updateProfile() {
 
     const handleUpdateProfile = () => {
         console.log("Atualizar perfil do usuário logado");
+    }
+
+    /* Trata evento de clique no Enter no botão de salvar alterações */
+    const handleKeyPressBtn = (e: React.KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === 'Enter') {
+            // Chama o evento de clique do botão
+            const { id } = e.target as HTMLDivElement;
+            const component = document.getElementById(id);
+            component?.click();
+        }
+    }
+
+    /* Trata evento de clique no Enter nas caixas de texto: manda foco para próxima caixa */
+    const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        const { id } = e.target as HTMLInputElement;
+        let component = null;
+
+        if (e.key === 'Enter') {
+            // Tecla enter foi clicada: coloca foco na próxima caixa de texto
+            if (id == 'email') {
+                // Foco vai para caixa de nome completo
+                component = document.getElementById('fullname');
+            } else if (id == 'fullname') {
+                // Foco vai para checkbox de mudança de senha
+                component = document.getElementById('changePassword');
+            } else if (id == 'password') {
+                // Foco vai para caixa de confirmação da nova senha
+                component = document.getElementById('passwordConfirm');
+            } else if (id == 'passwordConfirm') {
+                // Foco vai para botão de salvar alterações
+                component = document.getElementById('updateProfile');
+            }
+            component?.focus();
+        }
     }
 
     return (
@@ -28,6 +62,7 @@ export default function updateProfile() {
                                 type='text'
                                 placeholder='Digite seu nome de usuário'
                                 tabIndex={0}
+                                readOnly={true}
                             ></input>
                         </div>
 
@@ -39,6 +74,7 @@ export default function updateProfile() {
                                 type='email'
                                 placeholder='Digite seu email'
                                 tabIndex={0}
+                                onKeyUp={handleKeyPress}
                             ></input>
                         </div>
                     </div>
@@ -51,6 +87,7 @@ export default function updateProfile() {
                                 type='text'
                                 placeholder='Digite seu nome completo'
                                 tabIndex={0}
+                                onKeyUp={handleKeyPress}
                             ></input>
                         </div>
                         <div className={styles.changePassChkBoxContainer}>
@@ -58,6 +95,7 @@ export default function updateProfile() {
                                 id='changePassword'
                                 type='checkbox'
                                 onChange={handleChangePass}
+                                tabIndex={0}
                             />
                             <label htmlFor='changePassword' className={styles.label}>Alterar senha</label>
                         </div>
@@ -72,6 +110,7 @@ export default function updateProfile() {
                                     type='password'
                                     placeholder='Digite sua nova senha'
                                     tabIndex={0}
+                                    onKeyUp={handleKeyPress}
                                 ></input>
                             </div>
 
@@ -83,6 +122,7 @@ export default function updateProfile() {
                                     type='password'
                                     placeholder='Confirmar nova senha'
                                     tabIndex={0}
+                                    onKeyUp={handleKeyPress}
                                 ></input>
                             </div>
                         </div>
@@ -93,6 +133,7 @@ export default function updateProfile() {
                                 className={styles.button}
                                 tabIndex={0}
                                 onClick={ handleUpdateProfile }
+                                onKeyUp={handleKeyPressBtn}
                             >
                             <p className={styles.buttonLbl}>Salvar alterações</p>
                         </div>
